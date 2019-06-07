ed8e702949a32924c2d9838a45b8b3598aedfe03
Srishti Sharma <38854039+Srishti325@users.noreply.github.com>
2019-06-07 16:54:13 -0700

    Add files via upload

---
+++ elif ladder (1).py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+s1 = int(input("enter s1 marks: "))
+s2 = int(input("enter s2 marks: "))
+s3 = int(input("enter s3 marks: "))
+s4 = int(input("enter s4 marks: "))
+s5 = int(input("enter s5 marks: "))
+g_m = s1+s2+s3+s4+s5
+avg = g_m/500
+per = avg*100
+if (per > 70):
+    print("A")
+elif(per > 60):
+    print("B")
+elif(per > 40):
+    print("C")
+else:
+    print("Better luck next time")
+
+
+# In[15]:
+
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+c = int(input("enter c: "))
+if (a < b):
+    small = a
+    if (a > c):
+        small = c
+else:
+    small = b
+    if(b > c):
+        small = c
+print("small", small)       
+        
+
